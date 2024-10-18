2ce5229b6458a9827387bccdd7ecbb38a1cb7b74
Antonioafj <antonioafj.edu@gmail.com>
2024-10-17 23:54:53 -0400

    criado classe param dbmigration procedure e views no BD
    Signed-off-by: Antonioafj <antonioafj.edu@gmail.com>

--- jdbc-sample/src/main/java/br/com/dio/Main.java
+++ jdbc-sample/src/main/java/br/com/dio/Main.java
@@ -1,6 +1,7 @@
 package br.com.dio;
 
-import br.com.dio.persistence.EmployeeDAO;
+import br.com.dio.persistence.EmployeeAuditDAO;
+import br.com.dio.persistence.EmployeeParamDAO;
 import br.com.dio.persistence.entity.EmployeeEntity;
 import org.flywaydb.core.Flyway;
 
@@ -10,7 +11,9 @@ import java.time.OffsetDateTime;
 
 public class Main {
 
-    private final static EmployeeDAO employeeDAO = new EmployeeDAO();
+    private final static EmployeeParamDAO employeeDAO = new EmployeeParamDAO();
+
+    private final  static EmployeeAuditDAO employeeAuditDAO = new EmployeeAuditDAO();
 
     public static void main(String[] args) {
 
@@ -18,27 +21,29 @@ public class Main {
                 .dataSource("jdbc:mysql://localhost/jdbc-sample", "root", "mysqldeveloper")
                 .load();
         flayway.migrate();
-//
-//        var insert = new EmployeeEntity();
-//        insert.setName("Josefh");
-//        insert.setSalary(new BigDecimal("4550"));
-//        insert.setBirthday(OffsetDateTime.now().minusYears(25));
-//        System.out.println(insert);
-//        employeeDAO.insert(insert);
-//        System.out.println(insert);
+
+        var insert = new EmployeeEntity();
+        insert.setName("Josefh'");
+        insert.setSalary(new BigDecimal("4550"));
+        insert.setBirthday(OffsetDateTime.now().minusYears(25));
+        System.out.println(insert);
+        employeeDAO.insertWithProcedure(insert);
+        System.out.println(insert);
 
        // employeeDAO.findAll().forEach(System.out::println);
-//
-//       // System.out.println(employeeDAO.findById(1));
-//
+
+       // System.out.println(employeeDAO.findById(1));
+
          var update = new EmployeeEntity();
         update.setId(12);
         update.setName("Nome De Gente");
         update.setSalary(new BigDecimal("8520"));
         update.setBirthday(OffsetDateTime.now().minusYears(35).minusDays(15));
-       employeeDAO.update(update);
+        employeeDAO.update(update);
 
-      //employeeDAO.delete(9);
+       employeeDAO.delete(insert.getId());
+
+        employeeAuditDAO.findAll().forEach(System.out::println);
 
     }
 }
